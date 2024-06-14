f2b7854b78c3ed71a3214c93f141ae94b0d6c390
Kiran <kirankumardn74@gmail.com>
2024-06-14 11:55:44 +0530

    input file updated in folder file

--- src/components/folder.js
+++ src/components/folder.js
@@ -3,6 +3,20 @@ function Folder({ explorer }) {
   console.log('from folder file', explorer);
   const [expand, setExpand] = useState(false);
 
+  const [showInputs, setShowInput] = useState({
+    visible: false,
+    isFolder: null,
+  });
+
+  const handleNewFolder = (e, isFolder) => {
+    e.stopPropagation(e);
+    setExpand(true);
+    setShowInput({
+      visible: true,
+      isFolder,
+    });
+  };
+
   if (explorer.isFolder) {
     return (
       <div style={{ marginTop: 5 }}>
@@ -13,12 +27,27 @@ function Folder({ explorer }) {
           }}
         >
           <span>📂{explorer.name}</span>
+          <div>
+            <button onClick={(e) => handleNewFolder(e, true)}>Folder +</button>
+            <button onClick={(e) => handleNewFolder(e, false)}>File +</button>
+          </div>
         </div>
         <div style={{ display: expand ? 'block' : 'none', paddingLeft: 25 }}>
+          {showInputs.visible && (
+            <div className='inputContainer'>
+              <span>{showInputs.isFolder ? '📂' : '📄'}</span>
+              <input
+                type='text'
+                onBlur={() => setShowInput({ ...showInputs, visible: false })}
+                className='inputContainer__input'
+                autoFocus
+              />
+            </div>
+          )}
           {explorer.items.map((exp) => {
             return (
               <span className='file'>
-                <Folder explorer={exp} />
+                <Folder explorer={exp} key={exp.id} />
               </span>
             );
           })}
